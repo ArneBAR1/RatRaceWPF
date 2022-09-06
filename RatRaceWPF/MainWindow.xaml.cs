ecf7706bc76d408ef8cb072af32742212ca9922b
Andreas Krejberg Deleuran <andr568j@edu.sde.dk>
2022-09-06 13:03:10 +0200

    Updated

--- RatRaceWPF/MainWindow.xaml.cs
+++ RatRaceWPF/MainWindow.xaml.cs
@@ -21,9 +21,41 @@ namespace RatRaceWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        RaceManager raceManager = new RaceManager();
         public MainWindow()
         {
             InitializeComponent();
+            int NumberRats = RNG.Range(2, 9);
+            string[] names = { "Lui", "Palle", "Humus", "Ost", "Paladin", "Bard", "Kartoffel", "Ratatouille", "Flæskesteg", "BrunSovs", "Bluey", "Rory" };
+            for (int i = 0; i <= NumberRats; i++)
+            {
+                string RatName = names[i];
+                raceManager.CreateRat(RatName);
+            }
+
+            List<GetRats> rats = new List<GetRats>();
+            foreach (var RatsInRats in raceManager.Rats)
+            {
+                rats.Add(new GetRats { Name = RatsInRats.Name, Type = RatsInRats.Type });
+                RunnersTable.ItemsSource = rats;
+            }
+        }
+        public class GetRats
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string PlayerName = "";
+            PlayerName = textbox_playername.Text;
+            int money = RNG.Range(100, 1000);
+            raceManager.CreatePlayer(PlayerName, money);
+            raceManager.CreatePlayer();
+            textbox_playermoney.Text = money.ToString();
+            ButtonInfo.IsEnabled = false;
+            textbox_playername.IsEnabled = false;
         }
     }
 }
